8c31c7b437c4255be14cb1915ce587bba8ff9619
yangxvlin <1768528843@qq.com>
2020-03-20 22:35:15 +1100

    change regex and add comments

--- src/TwitterData.py
+++ src/TwitterData.py
@@ -7,11 +7,18 @@ Description: class for twitter data
 
 import json
 import re
+from string import punctuation
 
 
 class TwitterData:
+    """ required Dada model for Twitter data to be processed """
+
     def __init__(self, data: str):
+        """
+        :param data: twitter string
+        """
         json_data = json.loads(data)
         self.language_code = json_data["doc"]["metadata"]["iso_language_code"]
-        # TODO is my regex correct?
-        self.hash_tags = tuple(map(lambda x: x.lower(), re.findall("#[a-zA-Z0-9]+", json_data["doc"]["text"])))
+        # 1. search hashtag starts with '#' followed by alphabet or numbers and end with a white space or a punctuation
+        # 2. turn it to lower case
+        self.hash_tags = tuple(map(lambda x: x[:-1].lower(), re.findall("#[a-zA-Z0-9]+[\s{}]".format(punctuation), json_data["doc"]["text"])))
